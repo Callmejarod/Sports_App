b2e7fba48f39b052d4d3dc097e7c8b47bb98b261
JustinSingsanavong <justinsin1740@gmail.com>
2024-10-17 15:55:15 -0700

    [Refactor] Added empty brackets for null values to address PR comments

--- players_stats.py
+++ players_stats.py
@@ -10,17 +10,17 @@ def get_player_stats():
 
         request = requests.get(api_url)
         request.raise_for_status()
-        player_data = request.json()\
+        player_data = request.json()
         
         player_stats_json_list = []
 
         for player in player_data:
             player_id = player.get("PlayerID")
             name = player.get("Name")
-            passing_yards = player.get("PassingYards")
-            recieving_yards = player.get("ReceivingYards")
-            rushing_yards = player.get("RushingYards")
-            touchdowns = player.get("Touchdowns")
+            passing_yards = player.get("PassingYards", [])
+            recieving_yards = player.get("ReceivingYards", [])
+            rushing_yards = player.get("RushingYards", [])
+            touchdowns = player.get("Touchdowns", [])
 
             player_json = {
                 "player_id":player_id,
