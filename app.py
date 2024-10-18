0d977cdd02d8fdd3d0fe986e8b4704a8275c65be
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2024-10-18 12:49:39 -0700

    [feat] Added players api to app route

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask import render_template
+from flask import jsonify
+from players import get_available_players
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
+@app.route('/api/players')
+def get_player_data():
+    parse_player_data = get_available_players()
+    return jsonify(parse_player_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
