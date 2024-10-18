ea5f733f373697f0939d31a6d56712e25352f3ff
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2024-10-18 13:18:00 -0700

    [feat] Added an api that retrieves only players who are on active rosters

--- players.py
+++ players.py
@@ -1,5 +1,4 @@
 import requests
-import datetime
 from dotenv import load_dotenv
 import os
 
@@ -35,12 +34,44 @@ def get_available_players():
 
             player_json_list.append(player_json)
 
-        print(player_json_list)
+        print(len(player_json_list))
 
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
 
     return player_json_list
 
+def get_active_players():
+
+    api_url = f"https://api.sportsdata.io/v3/nfl/scores/json/PlayersByAvailable?key={os.getenv('api_key')}"
+
+    request = requests.get(api_url)
+    request.raise_for_status()
+    player_data = request.json()
+
+    player_list = []
+
+    for player in player_data:
+        player_id = player.get("PlayerID", [])
+        first_name = player.get("FirstName", [])
+        last_name =  player.get("LastName", [])
+        position = player.get("Position", [])
+        team = player.get("Team", [])
+
+        if team is not None:
+            player_json = {
+                    "player_id": player_id,
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "position": position,
+                    "team": team            
+            }
+
+            player_list.append(player_json)
+
+    print(len(player_list))
+
+    return player_list
 configure()
 get_available_players()
+get_active_players()
